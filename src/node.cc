f3ebceef7f99d78ae4b97d282d7e0cfa7a465f95
Cheng Zhao <zcbenz@gmail.com>
2013-03-18 22:32:07 +0800

    Add flags to control whether to set low level hooks and disable exceptions.

--- src/node.cc
+++ src/node.cc
@@ -122,6 +122,9 @@ using v8::V8;
 using v8::Value;
 using v8::kExternalUint32Array;
 
+bool g_standalone_mode = true;
+bool g_upstream_node_mode = true;
+
 static bool print_eval = false;
 static bool force_repl = false;
 static bool trace_deprecation = false;
@@ -1058,6 +1061,7 @@ Handle<Value> MakeCallback(Environment* env,
   env->tick_callback_function()->Call(process, 0, nullptr);
   CHECK_EQ(env->context(), env->isolate()->GetCurrentContext());
 
+  if (!g_standalone_mode) try_catch.Reset();
   if (try_catch.HasCaught()) {
     return Undefined(env->isolate());
   }
@@ -1084,6 +1088,7 @@ Handle<Value> MakeCallback(Environment* env,
 
   tick_info->set_in_tick(false);
 
+  if (!g_standalone_mode) try_catch.Reset();
   if (try_catch.HasCaught()) {
     tick_info->set_last_threw(true);
     return Undefined(env->isolate());
@@ -2793,8 +2798,12 @@ static void RawDebug(const FunctionCallbackInfo<Value>& args) {
 void LoadEnvironment(Environment* env) {
   HandleScope handle_scope(env->isolate());
 
+  if (g_upstream_node_mode) {  // No indent to minimize diff.
   env->isolate()->SetFatalErrorHandler(node::OnFatalError);
+  }  // g_upstream_node_mode
+  if (g_standalone_mode) {  // No indent to minimize diff.
   env->isolate()->AddMessageListener(OnMessage);
+  }  // g_standalone_mode
 
   // Compile, execute the src/node.js file. (Which was included as static C
   // string in node_natives.h. 'natve_node' is the string containing that
@@ -3345,6 +3354,7 @@ void Init(int* argc,
   // Initialize prog_start_time to get relative uptime.
   prog_start_time = static_cast<double>(uv_now(uv_default_loop()));
 
+  if (g_upstream_node_mode) {  // No indent to minimize diff.
   // Make inherited handles noninheritable.
   uv_disable_stdio_inheritance();
 
@@ -3419,6 +3429,7 @@ void Init(int* argc,
   }
 
   V8::SetArrayBufferAllocator(&ArrayBufferAllocator::the_singleton);
+  }  // g_upstream_node_mode
 
 #ifdef __POSIX__
   // Raise the open file descriptor limit.
@@ -3443,11 +3454,15 @@ void Init(int* argc,
       } while (min + 1 < max);
     }
   }
+  if (g_upstream_node_mode) {  // No indent to minimize diff.
   // Ignore SIGPIPE
   RegisterSignalHandler(SIGPIPE, SIG_IGN);
   RegisterSignalHandler(SIGINT, SignalExit, true);
   RegisterSignalHandler(SIGTERM, SignalExit, true);
+  }  // g_upstream_node_mode
 #endif  // __POSIX__
+
+  if (!g_upstream_node_mode) use_debug_agent = false;
 
   if (!use_debug_agent) {
     RegisterDebugSignalHandler();
