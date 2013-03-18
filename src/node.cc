294c8944e56eedc90ed23f8eb884f00aeefcd36c
Cheng Zhao <zcbenz@gmail.com>
2013-03-18 22:32:07 +0800

    Add flags to control whether to set low level hooks and disable exceptions.

--- src/node.cc
+++ src/node.cc
@@ -172,6 +172,9 @@ using v8::Value;
 
 using AsyncHooks = node::Environment::AsyncHooks;
 
+bool g_standalone_mode = true;
+bool g_upstream_node_mode = true;
+
 static bool print_eval = false;
 static bool force_repl = false;
 static bool syntax_check_only = false;
@@ -1236,7 +1239,9 @@ void SetupPromises(const FunctionCallbackInfo<Value>& args) {
 
   CHECK(args[0]->IsFunction());
 
+  if (g_standalone_mode) {  // No indent to minimize diff.
   isolate->SetPromiseRejectCallback(PromiseRejectCallback);
+  }
   env->set_promise_reject_function(args[0].As<Function>());
 
   env->process_object()->Delete(
@@ -3707,6 +3712,13 @@ static void RawDebug(const FunctionCallbackInfo<Value>& args) {
 }
 
 void LoadEnvironment(Environment* env) {
+  if (g_standalone_mode) {
+    env->isolate()->AddMessageListener(OnMessage);
+  }
+  if (g_upstream_node_mode) {
+    env->isolate()->SetFatalErrorHandler(OnFatalError);
+  }
+
   HandleScope handle_scope(env->isolate());
 
   TryCatch try_catch(env->isolate());
@@ -4530,7 +4542,9 @@ void Init(int* argc,
   node::RegisterBuiltinModules();
 
   // Make inherited handles noninheritable.
+  if (g_upstream_node_mode) {  // No indent to minimize diff.
   uv_disable_stdio_inheritance();
+  }  // g_upstream_node_mode
 
 #if defined(NODE_V8_OPTIONS)
   // Should come before the call to V8::SetFlagsFromCommandLine()
@@ -4588,6 +4602,7 @@ void Init(int* argc,
   }
 #endif
 
+  if (g_upstream_node_mode) {  // No indent to minimize diff.
   ProcessArgv(argc, argv, exec_argc, exec_argv);
 
 #if defined(NODE_HAVE_I18N_SUPPORT)
@@ -4604,6 +4619,7 @@ void Init(int* argc,
     exit(9);
   }
 #endif
+  }  // g_upstream_node_mode
 
   // Unconditionally force typed arrays to allocate outside the v8 heap. This
   // is to prevent memory pointers from being moved around that are returned by
