e80910efe740d0786615f019ab78be1afe7460e5
Cheng Zhao <zcbenz@gmail.com>
2013-08-27 18:38:41 +0800

    Don't set wrapper class in node, otherwise heap snapshot would crash.

--- src/smalloc.cc
+++ src/smalloc.cc
@@ -607,7 +607,9 @@ void Initialize(Handle<Object> exports,
                Uint32::NewFromUnsigned(env->isolate(), kMaxType));
 
   HeapProfiler* heap_profiler = env->isolate()->GetHeapProfiler();
+#if 0
   heap_profiler->SetWrapperClassInfoProvider(ALLOC_ID, WrapperInfo);
+#endif
 }
 
 
