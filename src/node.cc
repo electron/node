c162ff97c6ec9f5bf8d48f2f655ed1ea70e3b0df
Cheng Zhao <zcbenz@gmail.com>
2014-02-28 00:33:20 +0800

    Guard against NULL returned by uv_err_name.
    The uv_err_name would return NULL on unknown system error, which then would
    crash V8 by creating string from NULL.

--- src/node.cc
+++ src/node.cc
@@ -866,7 +866,11 @@ Local<Value> UVException(Isolate* isolate,
   if (!msg || !msg[0])
     msg = uv_strerror(errorno);
 
-  Local<String> js_code = OneByteString(isolate, uv_err_name(errorno));
+  const char* err_name = uv_err_name(errorno);
+  if (err_name == NULL)
+    err_name = "UnknownSystemError";
+
+  Local<String> js_code = OneByteString(isolate, err_name);
   Local<String> js_syscall = OneByteString(isolate, syscall);
   Local<String> js_path;
   Local<String> js_dest;
