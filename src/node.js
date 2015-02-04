9140160f9d3792dfd0f1de13a9781e28a9dc4d3c
Cheng Zhao <zcbenz@gmail.com>
2015-02-04 10:44:33 -0800

    Init asar support in Node

--- src/node.js
+++ src/node.js
@@ -32,6 +32,8 @@
     startup.globalConsole();
     }  // not isRenderer
 
+    startup.initAsarSupport();
+
     startup.processAssert();
     startup.processConfig();
     startup.processNextTick();
@@ -867,6 +869,10 @@
   NativeModule.prototype.cache = function() {
     NativeModule._cache[this.id] = this;
   };
+
+  startup.initAsarSupport = function() {
+    process.binding('atom_common_asar').initAsarSupport(process, NativeModule.require);
+  }
 
   startup();
 });
