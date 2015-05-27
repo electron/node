1bbfcd668e33306ea78bcf463839867e35274945
Cheng Zhao <zcbenz@gmail.com>
2015-05-27 15:25:58 +0800

    Don't handle external strings

--- src/string_bytes.cc
+++ src/string_bytes.cc
@@ -279,6 +279,7 @@ bool StringBytes::GetExternalParts(Isolate* isolate,
     return true;
   }
 
+#if 0
   if (!val->IsString())
     return false;
 
@@ -298,6 +299,7 @@ bool StringBytes::GetExternalParts(Isolate* isolate,
     *len = ext->length() * sizeof(*ext->data());
     return true;
   }
+#endif
 
   return false;
 }
