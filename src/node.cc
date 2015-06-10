50f1471de363934657b015222316b13917db9279
Cheng Zhao <zcbenz@gmail.com>
2015-06-10 14:10:48 +0800

    Don't create default V8::Platform

--- src/node.cc
+++ src/node.cc
@@ -4360,11 +4360,15 @@ static void StartNodeInstance(void* arg) {
       SealHandleScope seal(isolate);
       bool more;
       do {
+#if 0
         v8::platform::PumpMessageLoop(default_platform, isolate);
+#endif
         more = uv_run(env->event_loop(), UV_RUN_ONCE);
 
         if (more == false) {
+#if 0
           v8::platform::PumpMessageLoop(default_platform, isolate);
+#endif
           EmitBeforeExit(env);
 
           // Emit `beforeExit` if the loop became alive either after emitting
@@ -4428,9 +4432,11 @@ int Start(int argc, char** argv) {
   V8::SetEntropySource(crypto::EntropySource);
 #endif
 
+#if 0
   default_platform = v8::platform::CreateDefaultPlatform(v8_thread_pool_size);
   V8::InitializePlatform(default_platform);
   V8::Initialize();
+#endif
 
   int exit_code = 1;
   {
@@ -4446,8 +4452,10 @@ int Start(int argc, char** argv) {
   }
   V8::Dispose();
 
+#if 0
   delete default_platform;
   default_platform = nullptr;
+#endif
 
   delete[] exec_argv;
   exec_argv = nullptr;
