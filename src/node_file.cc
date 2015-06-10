df5620a8395aee306c2674db759600af53fe1691
Cheng Zhao <zcbenz@gmail.com>
2015-06-10 13:55:04 +0800

    Add fs.statSyncNoException
    This is now mainly for history reason.

--- src/node_file.cc
+++ src/node_file.cc
@@ -566,6 +566,26 @@ static void Stat(const FunctionCallbackInfo<Value>& args) {
   }
 }
 
+static void StatNoException(const FunctionCallbackInfo<Value>& args) {
+  Environment* env = Environment::GetCurrent(args.GetIsolate());
+  HandleScope scope(env->isolate());
+
+  if (args.Length() < 1 || !args[0]->IsString()) {
+    args.GetReturnValue().Set(v8::Boolean::New(env->isolate(), false));
+    return;
+  }
+
+  String::Utf8Value path(args[0]);
+
+  fs_req_wrap req_wrap;
+  int result = uv_fs_stat(uv_default_loop(), &req_wrap.req, *path, NULL);
+  if (result < 0)
+    args.GetReturnValue().Set(v8::Boolean::New(env->isolate(), false));
+  else
+    FillStatsArray(env->fs_stats_field_array(),
+                   static_cast<const uv_stat_t*>(SYNC_REQ.ptr));
+}
+
 static void LStat(const FunctionCallbackInfo<Value>& args) {
   Environment* env = Environment::GetCurrent(args);
 
@@ -582,6 +602,26 @@ static void LStat(const FunctionCallbackInfo<Value>& args) {
     FillStatsArray(env->fs_stats_field_array(),
                    static_cast<const uv_stat_t*>(SYNC_REQ.ptr));
   }
+}
+
+static void LStatNoException(const FunctionCallbackInfo<Value>& args) {
+  Environment* env = Environment::GetCurrent(args.GetIsolate());
+  HandleScope scope(env->isolate());
+
+  String::Utf8Value path(args[0]);
+
+  if (args.Length() < 1 || !args[0]->IsString()) {
+    args.GetReturnValue().Set(v8::Boolean::New(env->isolate(), false));
+    return;
+  }
+
+  fs_req_wrap req_wrap;
+  int result = uv_fs_lstat(uv_default_loop(), &req_wrap.req, *path, NULL);
+  if (result < 0)
+    args.GetReturnValue().Set(v8::Boolean::New(env->isolate(), false));
+  else
+    FillStatsArray(env->fs_stats_field_array(),
+                   static_cast<const uv_stat_t*>(SYNC_REQ.ptr));
 }
 
 static void FStat(const FunctionCallbackInfo<Value>& args) {
@@ -1431,7 +1471,9 @@ void InitFs(Local<Object> target,
   env->SetMethod(target, "internalModuleReadFile", InternalModuleReadFile);
   env->SetMethod(target, "internalModuleStat", InternalModuleStat);
   env->SetMethod(target, "stat", Stat);
+  env->SetMethod(target, "statNoException", StatNoException);
   env->SetMethod(target, "lstat", LStat);
+  env->SetMethod(target, "lstatNoException", LStatNoException);
   env->SetMethod(target, "fstat", FStat);
   env->SetMethod(target, "link", Link);
   env->SetMethod(target, "symlink", Symlink);
