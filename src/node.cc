a038b70c7bdd5b95c2f07c1c67430d9616ad5abe
Kevin Sawicki <kevinsawicki@gmail.com>
2017-07-11 14:35:15 -0700

    Comment out TryCatch::IsVerbose call

--- src/node.cc
+++ src/node.cc
@@ -2713,9 +2713,9 @@ void FatalException(Isolate* isolate,
 
 void FatalException(Isolate* isolate, const TryCatch& try_catch) {
   HandleScope scope(isolate);
-  if (!try_catch.IsVerbose()) {
+  // if (!try_catch.IsVerbose()) {
     FatalException(isolate, try_catch.Exception(), try_catch.Message());
-  }
+  // }
 }
 
 
