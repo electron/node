584e6370849aa73025e6206109f25f441cb6cf9f
Cheng Zhao <zcbenz@gmail.com>
2018-03-15 11:23:24 +0900

    Use 60 as module version for V8 6.3
    Numbers are in sync with Node.js:
    https://github.com/nodejs/node/blob/master/src/node_version.h#L96-L108

--- src/node_version.h
+++ src/node_version.h
@@ -106,7 +106,7 @@
  *
  * More information can be found at https://nodejs.org/en/download/releases/
  */
-#define NODE_MODULE_VERSION 59
+#define NODE_MODULE_VERSION 60
 
 // the NAPI_VERSION provided by this version of the runtime
 #define NAPI_VERSION  2
