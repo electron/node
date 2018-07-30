4c7a76bb2c0f4c1254e97bc0e5ac835136a04c89
Shelley Vohr <shelley.vohr@gmail.com>
2018-07-30 13:08:34 -0700

    fix: expose TraceEventHelper
    Node required tracing controller to be initialized, otherwise a crash would occur. Changed in concert with https://github.com/electron/electron/commit/0e5b6f93000e4718c9e35332ddbd0f6b76cdd585\#diff-0cdfa12fff513e022fac830c6af9c19a

--- src/tracing/trace_event.h
+++ src/tracing/trace_event.h
@@ -308,7 +308,7 @@ const int kZeroNumArgs = 0;
 const decltype(nullptr) kGlobalScope = nullptr;
 const uint64_t kNoId = 0;
 
-class TraceEventHelper {
+class NODE_EXTERN TraceEventHelper {
  public:
   static v8::TracingController* GetTracingController();
   static void SetTracingController(v8::TracingController* controller);
