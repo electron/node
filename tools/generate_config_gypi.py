88b494191c2a5b50b01dab80cd61ba3c0e0fbeb9
Jeremy Apthorp <jeremya@chromium.org>
2018-08-15 11:52:28 -0700

    build: add GN build files

---
+++ tools/generate_config_gypi.py
@@ -0,0 +1,11 @@
+# TODO: assess which if any of the config variables are important to include in
+# the js2c'd config.gypi.
+import sys
+
+def main(args):
+  out = args[0]
+  with open(out, 'w') as f:
+    f.write("{'variables':{}}\n")
+
+if __name__ == '__main__':
+  main(sys.argv[1:])
