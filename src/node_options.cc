a59b56f8812d6c430847b1f8745ea9c07c1a0831
Jeremy Apthorp <nornagon@nornagon.net>
2018-09-17 16:58:22 -0700

    fix: export DebugOptions

--- src/node_options.cc
+++ src/node_options.cc
@@ -55,6 +55,8 @@ namespace options_parser {
 // doc/api/cli.md
 // TODO(addaleax): Make that unnecessary.
 
+template class EXPORT_TEMPLATE_DEFINE(NODE_EXTERN) OptionsParser<DebugOptions>;
+
 DebugOptionsParser::DebugOptionsParser() {
   AddOption("--inspect-port",
             "set host:port for inspector",
