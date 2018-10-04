ba88ff2df82032ddd19a9e24508f08c4cd423a32
Samuel Attard <samuel.r.attard@gmail.com>
2018-10-04 10:08:23 +1000

    fix: tar.py is somehow broken

--- tools/tar.py
+++ tools/tar.py
@@ -1,11 +1,12 @@
 import os
 import sys
 import tarfile
- source = sys.argv[1]
+
+source = sys.argv[1]
 target = sys.argv[2]
- os.chdir(os.path.dirname(source))
- print(os.getcwd())
-print(os.path.basename(target), source, os.path.relpath(source))
- tarball = tarfile.open(name=os.path.basename(target), mode='w:gz')
+
+os.chdir(os.path.dirname(source))
+
+tarball = tarfile.open(name=os.path.basename(target), mode='w:gz')
 tarball.add(os.path.relpath(source))
 tarball.close()
