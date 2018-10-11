78e956e08a250c4743b7b27371ed38106020e25a
Samuel Attard <sattard@atlassian.com>
2018-10-11 13:09:47 +1100

    chore: remove logging from tar.py

--- tools/tar.py
+++ tools/tar.py
@@ -7,9 +7,6 @@ target = sys.argv[2]
 
 os.chdir(os.path.dirname(source))
 
-print(os.getcwd())
-print(os.path.basename(target), source, os.path.relpath(source))
-
 tarball = tarfile.open(name=os.path.basename(target), mode='w:gz')
 tarball.add(os.path.relpath(source))
 tarball.close()
