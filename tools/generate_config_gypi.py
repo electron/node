aa79b3c3d5b456f75fa91750667b67f23d9c8a3a
Jeremy Apthorp <jeremya@chromium.org>
2019-06-10 16:51:02 -0700

    fix: set node_with_ltcg=true in config.gypi on Windows

--- tools/generate_config_gypi.py
+++ tools/generate_config_gypi.py
@@ -1,11 +1,18 @@
 # TODO: assess which if any of the config variables are important to include in
 # the js2c'd config.gypi.
 import sys
+import json
 
-def main(args):
-  out = args[0]
+def main(out, target_os):
+  config = {
+    'variables': {
+      'built_with_electron': 1,
+    }
+  }
+  if target_os == 'win':
+    config['variables']['node_with_ltcg'] = 'true'
   with open(out, 'w') as f:
-    f.write("{'variables':{'built_with_electron': 1}}\n")
+    json.dump(config, f, sort_keys=True)
 
 if __name__ == '__main__':
-  main(sys.argv[1:])
+  main(*sys.argv[1:])
